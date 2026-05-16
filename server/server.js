c1ef177e2736ba6f46ee0b9c63d6ce89fca6e3ae
Nitin <nitingarhwal184@gmail.com>
2026-05-16 11:26:30 +0530

    Fixed backend

--- server/server.js
+++ server/server.js
@@ -1,25 +1,51 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/messages", require("./routes/messageRoutes"));
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Backend + MongoDB Running 🚀");
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const Message = mongoose.model("Message", messageSchema);
+
+app.post("/api/messages", async (req, res) => {
+  try {
+    const newMessage = new Message(req.body);
+    await newMessage.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Message Saved Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
