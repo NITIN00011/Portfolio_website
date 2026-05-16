e0480a7672fecb8cfb2b23b3f7bd49093a386054
Nitin <nitingarhwal184@gmail.com>
2026-05-16 10:43:08 +0530

    Initial portfolio website

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Message = require("../models/Message");
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+
+    const newMessage = new Message({
+      name,
+      email,
+      message,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Message Saved Successfully 🚀",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
+
+module.exports = router;
